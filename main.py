31b5e2d318d744c5bce908d2eb4bc9c2e264ae0e
ApplessTracy <tracy123456111@yahoo.com>
2022-05-27 14:38:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -69,17 +69,7 @@ def move():
                 else:
                     return moves[0]
         
-        if playerX == myX:
-            if playerY[i] < myY and myY-3 <= playerY[i]:
-                if myFace != 'W':
-                    return moves[3]
-                else:
-                    return moves[0]
-            elif myY < playerY[i] and playerY[i] <= myY-3:
-                if myFace != 'E':
-                    return moves[3]
-                else:
-                    return moves[0]
+       
 
         else:
             return moves[random.randrange(2,4)]
